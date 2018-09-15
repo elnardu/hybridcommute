51617a1dfac347319fc76d21abaa1ce65ba27d2e
elnardu <elnardu2@gmail.com>
2018-09-15 16:55:52 -0400

    Add aggregations

--- db/aggregations.py
+++ db/aggregations.py
@@ -1,5 +1,34 @@
 from pymongo import MongoClient, GEOSPHERE
-from bs4 import BeautifulSoup
 
 db = MongoClient('localhost', 27017).hybridcommute
 col = db.locations
+
+
+def getNearest(type_: str, lon: float, lat: float):
+    return col.find_one({
+        'type': type_,
+        'loc': {
+            '$near': {
+                '$geometry': {
+                    'type': "Point",
+                    'coordinates': [lon, lat]
+                },
+                '$maxDistance': 10000
+            }
+        }
+    })
+
+# print(getNearest('car_parking', -71.07916, 42.34840))
+# print(getNearest('bicycle_rental', -71.07916, 42.34840))
+
+def getInRange(type_: str, lon: float, lat: float, radius: float):
+    return list(col.find({
+        'type': type_,
+        'loc': {
+            '$geoWithin': {
+                '$centerSphere': [[lon, lat], radius / (6378.1 * 1000)]
+            }
+        }
+    }))
+
+# print(getInRange('bicycle_rental', -71.077303, 42.349673, 20))
