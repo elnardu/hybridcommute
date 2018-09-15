21fb68fbacb301afcdc2154ef904c92419130cb2
elnardu <elnardu2@gmail.com>
2018-09-15 17:56:20 -0400

    New api routes

--- api.py
+++ api.py
@@ -1,8 +1,36 @@
-from flask import Flask
+from db.aggregations import getInRange, getNearest
+from bson import json_util 
+
+from flask import Flask, jsonify, request, Response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/api/getInRange", methods=['POST'])
+def getInRangeRoute():
+    req = request.get_json()
+    res = getInRange(
+        req['type'],
+        float(req['lon']),
+        float(req['lat']),
+        float(req['radius'])
+    )
+    return Response(response=json_util.dumps(res),
+                    status=200,
+                    mimetype="application/json")
+
+@app.route("/api/getNearest", methods=['POST'])
+def getNearestRoute():
+    req = request.get_json()
+    res = getNearest(
+        req['type'],
+        float(req['lon']),
+        float(req['lat'])
+    )
+    return Response(response=json_util.dumps(res),
+                    status=200,
+                    mimetype="application/json")
 
 app.run(host='0.0.0.0', debug=True)
